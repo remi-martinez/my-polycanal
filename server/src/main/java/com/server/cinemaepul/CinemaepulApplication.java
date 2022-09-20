6fd71074f7403f2d83b0ad28f40e1231bf3a281f
Rémi Martinez <remi.martinez74500@outlook.fr>
2022-09-20 10:44:30 +0200

    feat(backend): initial commit

---
+++ server/src/main/java/com/server/cinemaepul/CinemaepulApplication.java
@@ -0,0 +1,13 @@
+package com.server.cinemaepul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaepulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaepulApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/server/cinemaepul/CinemaepulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.cinemaepul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaepulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
