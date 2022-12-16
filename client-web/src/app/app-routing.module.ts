dc05173af4eda91e8f4829fca31baf6e6d257e4a
anaisthoral <anaisthoral@gmail.com>
2022-12-16 11:48:51 +0100

    feat: navbarre

--- client-web/src/app/app-routing.module.ts
+++ client-web/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component:HeaderComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-web/src/app/app.component.ts
+++ client-web/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client-web';
+export class AppComponent implements OnInit{
+
+  ngOnInit() {
+
+}
 }
