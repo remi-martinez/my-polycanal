13abc44549b1b697e6119c3993578806eae2580b
anaisthoral <anaisthoral@gmail.com>
2022-12-23 12:10:32 +0100

    feat: finalisation navbarre

--- client-web/src/app/header/header.component.ts
+++ client-web/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  estAccueilActif = false;
+  estEnDirectActif = false;
+  estProgrammeTVActif = false;
+  estChaineAppsActif = false;
+  estVideosActif = false;
+  //on récupère le chemin de la route active
+  cheminActuel = this.route.snapshot.routeConfig?.path;
+
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Définissions de la variable appropriée en fonction de la page active
+    // @ts-ignore
+    if (this.estSurAccueil()) {
+      this.estAccueilActif = true;
+    }
+  }
+
+  estSurAccueil() {
+    return this.cheminActuel==='';
+  }
 
 }
