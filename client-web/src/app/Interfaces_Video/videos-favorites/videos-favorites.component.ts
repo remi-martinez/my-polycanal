ea05e36c2506e8d2ff60d3e8ec41af1a9639978f
anaisthoral <anaisthoral@gmail.com>
2023-01-08 10:38:53 +0100

    feat: click sur les categories

--- client-web/src/app/Interfaces_Video/videos-favorites/videos-favorites.component.ts
+++ client-web/src/app/Interfaces_Video/videos-favorites/videos-favorites.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-videos-favorites',
@@ -7,7 +8,14 @@ import { Component } from '@angular/core';
 })
 export class VideosFavoritesComponent {
 
+  constructor(private router: Router) { }
+
   showPopUp!: boolean;
   showOverlay!: boolean;
   showFav: boolean = true;
+
+  goCategorieFilms() {
+    this.router.navigateByUrl('categories');
+  }
+
 }
--- client-web/src/app/Interfaces_Video/videos/videos.component.ts
+++ client-web/src/app/Interfaces_Video/videos/videos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-videos',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./videos.component.scss']
 })
 export class VideosComponent {
+
+  constructor(private router: Router) { }
+
   showPopUp!: boolean;
   showOverlay!: boolean;
   showFav: boolean = false;
+
+  goCategorieFilms() {
+    this.router.navigateByUrl('categories');
+  }
+
 }
