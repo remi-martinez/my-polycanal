9c7781bbbd1b9a62f52ffbf6694c62a0f9e6a2fd
Alexandre <herstalex@gmail.com>
2023-02-27 11:32:21 +0100

    WIP

---
+++ client-web/src/app/Gestion_Video/ajout-personnage/ajout-personnage.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-ajout-personnage',
+  template: `
+  <div>
+
+  </div>
+  `,
+  styles: [``]
+})
+export class AjoutPersonnageComponent implements OnInit {
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+}
---
+++ client-web/src/app/services/acteur.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Acteur} from "../models/acteur";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActeurService {
+  private apiUrl = 'http://localhost:8080/acteurs';
+
+  constructor(private http: HttpClient) { }
+
+  getActeurs(): Observable<Acteur[]> {
+    return this.http.get<Acteur[]>(this.apiUrl);
+  }
+
+  getActeur(id: number): Observable<Acteur> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<Acteur>(url);
+  }
+
+  addActeur(acteur: Acteur): Observable<Acteur> {
+    return this.http.post<Acteur>(this.apiUrl, acteur);
+  }
+
+  updateActeur(acteurId: string, acteur: Acteur): Observable<Acteur> {
+    const url = `${this.apiUrl}/${acteurId}`;
+    return this.http.put<Acteur>(url, acteur);
+  }
+
+  deleteActeur(id: number): Observable<Acteur> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<Acteur>(url);
+  }
+}
