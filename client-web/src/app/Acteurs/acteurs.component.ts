b38673338d406138e3541915b552142886e21f91
AUBERT Baptiste <bama7000@gmail.com>
2023-03-01 13:02:06 +0100

    feat: show pop up acteur + info personnages

--- client-web/src/app/Acteurs/acteurs.component.ts
+++ client-web/src/app/Acteurs/acteurs.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute, NavigationEnd, ParamMap, Router, RouterEvent} from "@ang
 import {Observable, Subject, takeUntil} from "rxjs";
 import {Acteur} from "../models/acteur";
 import {ActeurService} from "../services/acteur.service";
+import {Film} from "../models/film";
+import {Personnage} from "../models/personnage";
+import {PersonnageAvecFilmDto} from "../models/personnageAvecFilmDto";
 
 @Component({
   selector: 'app-acteurs',
@@ -14,14 +17,18 @@ export class ActeursComponent implements OnInit{
   acteurs$: Observable<Acteur[]> | undefined;
   acteursObserver: any;
   acteurs: Acteur[] | undefined;
-  film: Acteur | undefined;
+  acteur: Acteur | undefined;
+  popUp!: boolean;
+  personnages$: Observable<PersonnageAvecFilmDto> | undefined;
+  personnagesObserver: any;
+  personnages: PersonnageAvecFilmDto[] | undefined;
 
   constructor(private router: Router, private route: ActivatedRoute, private acteurService: ActeurService) {
   }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe((paramMap: ParamMap) => {
-      this.acteurs$ = this.acteurService.getAllActeursWithPersonnages();
+      this.acteurs$ = this.acteurService.getActeurs();
 
       this.acteursObserver = {
         next: (f: Acteur[]) => {
@@ -33,5 +40,19 @@ export class ActeursComponent implements OnInit{
       this.acteurs$.subscribe(this.acteursObserver)
     });
 
+  }
+
+  showPopUp(acteur: Acteur) {
+    this.popUp = true;
+    this.acteur = acteur;
+    this.personnages$ = this.acteurService.getPersonnagesByActeurId(acteur.id);
+    this.personnagesObserver = {
+      next: (p: PersonnageAvecFilmDto[]) => {
+        this.personnages = p;
+      },
+      error: (err: Error) => console.error("Error while fetching : " + err),
+      complete: () => console.log(this.personnages)
+    }
+    this.personnages$.subscribe(this.personnagesObserver)
   }
 }
--- server/src/main/java/com/server/cinemaepul/acteur/ActeurController.java
+++ server/src/main/java/com/server/cinemaepul/acteur/ActeurController.java
@@ -1,5 +1,7 @@
 package com.server.cinemaepul.acteur;
 
+import com.server.cinemaepul.personnage.PersonnageAvecFilmDto;
+import com.server.cinemaepul.personnage.PersonnageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,9 @@ import java.util.List;
 public class ActeurController {
     @Autowired
     private ActeurService acteurService;
+
+    @Autowired
+    private PersonnageService personnageService;
 
     @GetMapping
     List<Acteur> findAll() {
@@ -35,5 +40,15 @@ public class ActeurController {
     @DeleteMapping("/{id}")
     public Integer delete(@PathVariable("id") Integer acteurId) {
         return acteurService.delete(acteurId);
+    }
+
+    @GetMapping("/{id}/personnages")
+    public List<PersonnageAvecFilmDto> getPersonnagesByActeur(@PathVariable("id") Integer acteurId) {
+        try {
+            return personnageService.getPersonnagesByActeur(acteurId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
