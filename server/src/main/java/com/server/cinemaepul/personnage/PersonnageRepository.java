e913e563fc88f9db6d84b3b6825d0a4c53ea2e65
AUBERT Baptiste <bama7000@gmail.com>
2023-03-01 15:15:11 +0100

    feat: personnages back

--- server/src/main/java/com/server/cinemaepul/personnage/PersonnageRepository.java
+++ server/src/main/java/com/server/cinemaepul/personnage/PersonnageRepository.java
@@ -1,7 +1,12 @@
 package com.server.cinemaepul.personnage;
 
+import com.server.cinemaepul.acteur.Acteur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonnageRepository extends JpaRepository<Personnage, PersonnageId> {
+
+    List<Personnage> findAllByActeur(Acteur acteur);
 }
 
